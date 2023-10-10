835c2b24a805e6f7253f3407e3c593d996b9b5a9
Marcus Risanger <69350948+MarcusRisanger@users.noreply.github.com>
2023-10-10 06:11:38 +0200

    Update _metadata_defs.py

--- dataverse_api/_metadata_defs.py
+++ dataverse_api/_metadata_defs.py
@@ -3,9 +3,27 @@ This contains some dataclasses for metadata handling.
 """
 
 from dataclasses import dataclass
-from typing import Any, Optional, Type
+from typing import Any, Literal, Type, Union
 
 BASE = "Microsoft.Dynamics.CRM."
+
+
+@dataclass
+class ManagedProperty:
+    """
+    Creates metadata for managed properties in Dataverse.
+    """
+
+    value: Union[str, bool]
+    can_be_changed: bool
+    managed_property_name: str
+
+    def __call__(self) -> dict[str, Any]:
+        return {
+            "Value": self.value,
+            "CanBeChanged": self.can_be_changed,
+            "ManagedPropertyLogicalName": self.managed_property_name,
+        }
 
 
 @dataclass
@@ -18,7 +36,7 @@ class LocalizedLabel:
     label: str
     languagecode: int
 
-    def __call__(self) -> dict:
+    def __call__(self) -> dict[str, Any]:
         return {
             "@odata.type": BASE + "LocalizedLabel",
             "Label": self.label,
@@ -27,7 +45,7 @@ class LocalizedLabel:
 
 
 @dataclass
-class Label:
+class Label(LocalizedLabel):
     """
     Create metadata for Label.
 
@@ -37,18 +55,10 @@ class Label:
     Default country code is 1033 (US English).
     """
 
-    label: Optional[str] = None
-    country_code: Optional[int] = 1033
-    labels: Optional[list[LocalizedLabel]] = None
-
-    def __call__(self):
-        if self.labels is not None:
-            self.localized_labels = self.labels
-        else:
-            self.localized_labels = [LocalizedLabel(self.label, self.country_code)]
+    def __call__(self) -> dict[str, Any]:
         return {
             "@odata.type": BASE + "Label",
-            "LocalizedLabels": [x() for x in self.localized_labels],
+            "LocalizedLabels": [LocalizedLabel(self.label, self.languagecode)()],
         }
 
 
@@ -62,17 +72,11 @@ class _BaseMetadata:
     display_name: Label
     schema_name: str
 
-
-@dataclass
-class RequiredLevel:
-    level: str = "None"
-    can_be_changed: bool = True
-
-    def __call__(self):
+    def _base_metadata(self):
         return {
-            "Value": self.level,
-            "CanBeChanged": self.can_be_changed,
-            "ManagedPropertyLogicalName": "canmodifyrequirementlevelsettings",
+            "Description": self.description(),
+            "DisplayName": self.display_name(),
+            "SchemaName": self.schema_name,
         }
 
 
@@ -80,64 +84,76 @@ class RequiredLevel:
 class AttributeMetadata(_BaseMetadata):
     """
     Create metadata for Attribute.
+    Includes metadata from BaseMetadata.
     """
 
-    required_level: RequiredLevel
+    required_level: ManagedProperty
+
+    def _attr_metadata(self) -> dict[str, Any]:
+        attr_metadata = {
+            "RequiredLevel": self.required_level(),
+        }
+        attr_metadata.update(self._base_metadata())
+        return attr_metadata
 
 
 @dataclass
 class StringAttributeMetadata(AttributeMetadata):
-    max_length: int
-    format_name: str
-    is_primary: bool = False
+    """
+    Create metadata for StringAttribute.
+    Includes metadata from AttributeMetadata and BaseMetadata.
+    """
 
-    def __call__(self) -> Any:
-        return {
+    format_name: Literal[
+        "Email",
+        "Text",
+        "TextArea",
+        "Url",
+        "TickerSymbol",
+        "PhoneticGuide",
+        "VersionNumber",
+        "Phone",
+        "Json",
+        "RichText",
+    ]
+    is_primary: bool
+    max_length: int
+
+    def __call__(self) -> dict[str, Any]:
+        base = {
             "@odata.type": BASE + "StringAttributeMetadata",
             "AttributeType": "String",
             "AttributeTypeName": {"Value": "StringType"},
-            "Description": self.description(),
-            "DisplayName": self.display_name(),
             "IsPrimaryName": self.is_primary,
-            "RequiredLevel": self.required_level(),
-            "SchemaName": self.schema_name,
-            "FormatName": self.format_name,
+            "FormatName": {"Value": self.format_name},
             "MaxLength": self.max_length,
         }
+        base.update(self._attr_metadata())
+        return base
 
 
 @dataclass
 class EntityMetadata(_BaseMetadata):
     """
     Create metadata for Entity.
-
-    Only one Attribute must have a IsPrimaryName = True property.
     """
 
+    attributes: Union[StringAttributeMetadata, list[Type[AttributeMetadata]]]
     display_collection_name: Label
-    primary_attribute: StringAttributeMetadata
-    ownership_type: str = "TeamOwned"
-    attributes: Optional[list[Type[AttributeMetadata]]] = None
-    has_activities: bool = False
-    has_notes: bool = False
-    is_activity: bool = False
+    ownership_type: str
+    is_activity: bool
+    has_activities: bool
+    has_notes: bool
 
     def __call__(self):
-        if self.attributes is not None:
-            self.attributes.append(self.primary_attribute)
-        else:
-            self.attributes = list()
-            self.attributes.append(self.primary_attribute)
-
-        return {
+        entity_metadata = {
             "@odata.type": BASE + "EntityMetadata",
             "Attributes": [x() for x in self.attributes],
-            "Description": self.description(),
             "DisplayCollectionName": self.display_collection_name(),
-            "DisplayName": self.display_name(),
+            "IsActivity": self.is_activity,
             "HasActivities": self.has_activities,
             "HasNotes": self.has_notes,
-            "IsActivity": self.is_activity,
             "OwnershipType": self.ownership_type,
-            "SchemaName": self.schema_name,
         }
+        entity_metadata.update(self._base_metadata())
+        return entity_metadata
