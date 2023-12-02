24392c7fb5bdf51038829e75f76f2ff7bd0a34c7
Marcus Risanger <69350948+MarcusRisanger@users.noreply.github.com>
2023-12-02 20:21:23 +0100

    Tests sorted so far

--- tests/test_dataverse.py
+++ tests/test_dataverse.py
@@ -5,6 +5,20 @@ import pytest
 import responses
 
 from dataverse.dataverse import Dataverse
+from dataverse.errors import DataverseError
+
+from responses.matchers import json_params_matcher
+
+
+def test_api_call(client: Dataverse, mocked_responses: responses.RequestsMock):
+    # Mocking an errored request
+
+    mocked_responses.get(url=f"{client._endpoint}Foo", status=500)
+
+    expected = "Error with GET request:"
+
+    with pytest.raises(DataverseError, match=expected):
+        client._Dataverse__api_call(method="get", url="Foo")
 
 
 def test_create_entity(
@@ -19,8 +33,8 @@ def test_create_entity(
         "url": f"{client._endpoint}EntityDefinitions",
         "status": 204,
         "content_type": "application/json",
+        "match": [json_params_matcher({"Test": "Data"})],
     }
-
     mocked_responses.post(**response)
 
     # Running function - this errors if the endpoint URL
@@ -51,3 +65,68 @@ def test_delete_entity(
     resp = client.delete_entity(logical_name=name)
 
     assert resp.request.body is None
+
+
+def test_create_publisher(
+    client: Dataverse,
+    mocked_responses: responses.RequestsMock,
+):
+    name, unique_name, description, prefix, option_prefix = "A", "B", "C", "D", 69
+
+    mocked_responses.post(
+        url=f"{client._endpoint}publishers",
+        status=204,
+        match=[
+            json_params_matcher(
+                {
+                    "friendlyname": name,
+                    "uniquename": unique_name,
+                    "description": description,
+                    "customizationprefix": prefix,
+                    "customizationoptionvalueprefix": option_prefix,
+                }
+            )
+        ],
+    )
+
+    resp = client.create_publisher(
+        publisher_name=name,
+        unique_name=unique_name,
+        description=description,
+        prefix=prefix,
+        option_prefix=option_prefix,
+    )
+
+    assert resp.status_code == 204
+
+
+def test_create_solution(
+    client: Dataverse,
+    mocked_responses: responses.RequestsMock,
+):
+    name, unique_name, description, guid = "A", "B", "C", "D"
+
+    mocked_responses.post(
+        url=f"{client._endpoint}solutions",
+        status=204,
+        match=[
+            json_params_matcher(
+                {
+                    "friendlyname": name,
+                    "uniquename": unique_name,
+                    "description": description,
+                    "version": "1.0.0.0",
+                    "publisher@odata.bind": f"publishers({guid})",
+                }
+            )
+        ],
+    )
+
+    resp = client.create_solution(
+        solution_name=name,
+        unique_name=unique_name,
+        description=description,
+        publisher_guid=guid,
+    )
+
+    assert resp.status_code == 204
