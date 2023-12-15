197c8fd7858d4599ced2ea643544b9fbabe9e4a6
Marcus Risanger <69350948+MarcusRisanger@users.noreply.github.com>
2023-12-15 23:24:11 +0100

    Starting to flesh out entity

--- dataverse/entity.py
+++ dataverse/entity.py
@@ -8,7 +8,24 @@ class DataverseEntity(Dataverse):
         super().__init__(session=session, environment_url=environment_url)
 
         self._logical_name = logical_name
+        self._entity_set_name = self.__get_entity_set_name()
 
     @property
     def logical_name(self) -> str:
         return self._logical_name
+
+    @property
+    def entity_set_name(self) -> str:
+        return self._entity_set_name
+
+    def __get_entity_set_name(self) -> str:
+        """
+        To fetch the Entity Set Name of the Entity, used
+        instead of Logical Name as API endpoint.
+        """
+        resp = self._api_call(
+            method="GET",
+            url=f"EntityDefinitions(LogicalName='{self.logical_name}')",
+            params={"$select": "EntitySetName"},
+        )
+        return resp.json()["EntitySetName"]
